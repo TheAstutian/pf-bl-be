13bd9a12b4c83af526e2dfc3ad8cf8d414becbb6
TheAstutian <speakingatoms@gmail.com>
2024-05-11 19:29:06 +0100

    delete route

--- routes.js
+++ routes.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import bcrypt from 'bcryptjs'
+import bcrypt from 'bcryptjs';
+import Mongo, { BSON } from'mongodb';
 import db from './db.js';
 
 import { ObjectId } from 'mongodb';
@@ -7,12 +8,29 @@ import { ObjectId } from 'mongodb';
 const router= express.Router(); //takes care of all requests
 
 router.get('/', async (req,res) =>{
-    const collection = await db.collection("users");
+    const collection =  db.collection("users");
     const results = await collection.find({}).toArray();
     res.send(results).status(200);
     
 })
+//get all posts
+router.get('/posts', async(req,res)=>{
+    const collection =  db.collection('items')
+    const results = await collection.find({}).toArray();
+    res.send(results).status(200);
+})
 
+//get single post
+
+router.post('/posts/:id', async(req,res)=>{
+    const id = req.body.id
+    const nid= new BSON.ObjectId(id)
+    const collection =  db.collection('items')
+    const item = await collection.findOne({
+        _id: nid
+    })
+   return res.status(200).json(item)
+})
 //api for register
 router.post('/register', async(req,res)=>{
 
@@ -24,7 +42,7 @@ router.post('/register', async(req,res)=>{
             username:req.body.username,
             password:passwordHash
         }
-        let collection = await db.collection('users');//create collection of users
+        let collection =  db.collection('users');//create collection of users
         const user = await collection.findOne({username:req.body.username})
         if(user) {
             return res.status(400).json("User already exists")
@@ -39,7 +57,7 @@ router.post('/register', async(req,res)=>{
 router.post('/login', async (req,res)=>{
  
     try{
-        const collection = await db.collection("users");
+        const collection =  db.collection("users");
         const user = await collection.findOne({
             username: req.body.username
         });
@@ -64,7 +82,7 @@ router.post('/login', async (req,res)=>{
 router.get('/post', async (req,res)=>{
 
     try{
-        const collection = await db.collection("items")
+        const collection =  db.collection("items")
         const post = await collection.findOne({
             _id: req.body.username
         })
@@ -79,7 +97,7 @@ router.get('/post', async (req,res)=>{
 
 router.get('/home', async(req,res)=>{
     try{
-    const collection = await db.collection('items')
+    const collection =  db.collection('items')
     const posts = await collection.find({}).toArray()
     res.send(posts).status(200);
     }catch(err){
@@ -100,10 +118,11 @@ router.post('/write', async (req,res)=>{
             quoter: req.body.quoter,
             model: req.body.model,
             imglnk:req.body.imglnk,
+            tags: req.body.tags,
             date:req.body.date
 
         }
-        let collection = await db.collection('items');//create collection of users
+        let collection =  db.collection('items');//create collection of users
         await collection.insertOne(newDocument);
         return res.status(200).json("new post created successfully")
     }catch(err){
@@ -111,6 +130,19 @@ router.post('/write', async (req,res)=>{
     }
 
 })
+
+//deletePost
+router.delete("/delete/:id", async (req,res)=>{
+    try{
+        const query = {_id: new ObjectId(req.params.id)}
+        const collection = db.collection("items")
+        let result = await collection.deleteOne(query);
+        res.send(result).status(200);
+    } catch(err){
+        console.log(err)
+    }
+})
+
 
 
 export default router; 
